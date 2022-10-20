00483620a64d323f9a2bf056d90346540405b4eb
Алексей Мамиконян <malekseyg@gmail.com>
2022-10-20 21:32:21 +0300

    - добавлен проект для хостинга основного проекта

---
+++ ReleaseTestServer/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
