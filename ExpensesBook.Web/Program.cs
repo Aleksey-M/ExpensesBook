c5062c4f9c1fe05ea4730df8182fb8c03ec98168
Алексей Мамиконян <malekseyg@gmail.com>
2022-11-21 21:28:25 +0300

    - переименован WASM-проект

---
+++ ExpensesBook.Web/Program.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using ExpensesBook.App;
+using ExpensesBook.Data;
+using ExpensesBook.Domain.Calculators;
+using ExpensesBook.Domain.Repositories;
+using ExpensesBook.Domain.Services;
+using ExpensesBook.LocalStorageRepositories;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace ExpensesBook.Web;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<MainApp>("#app");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddMudServices();
+
+        #region register services
+
+        AddRepositories(builder.Services);
+
+        AddAppServices(builder.Services);
+
+        builder.Services.AddScoped<IGlobalDataManager, GlobalDataManager>();
+
+        AddCalculators(builder.Services);
+
+        #endregion
+
+        await builder.Build().RunAsync();
+    }
+
+    private static void AddCalculators(IServiceCollection services)
+    {
+        services.AddScoped<PeriodExpenseCalculator>();
+        services.AddScoped<CashBalanceCalculator>();
+        services.AddScoped<LimitsCalculator>();
+        services.AddScoped<YearExpensesCalculator>();
+    }
+
+    private static void AddAppServices(IServiceCollection services)
+    {
+        services.AddScoped<ICategoriesService, CategoriesService>();
+        services.AddScoped<IGroupsService, GroupsService>();
+        services.AddScoped<IExpensesService, ExpensesService>();
+        services.AddScoped<IIncomesService, IncomesService>();
+        services.AddScoped<ILimitsService, LimitsService>();
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<ICategoriesRepository, CategoriesRepository>();
+        services.AddScoped<IGroupsRepository, GroupsRepository>();
+        services.AddScoped<IGroupDefaultCategoryRepository, GroupDefaultCategoryRepository>();
+        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+        services.AddScoped<IIncomesRepository, IncomesRepository>();
+        services.AddScoped<ILimitsRepository, LimitsRepository>();
+    }
+}
